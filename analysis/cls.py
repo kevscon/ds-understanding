1d50bba46b2f6ff966eaf6951810ec9702fdffc3
kevcon <kevscon@gmail.com>
2019-08-21 21:31:10 -0400

    updates

---
+++ analysis/cls.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+class Test:
+
+    def __init__(self):
+        pass
+
+    def testy(data1, data2):
+        df = pd.DataFrame({'a': data1, 'b': data2})
+        return(df)
+    # def __init__(self, data):
+    #     self.data = data
+    #
+    # def frame_it(self):
+        # return pd.DataFrame(data)
