9139eca86b9618bb8cf2cafb8c36d24e9f4ccc7e
kevcon <kevscon@gmail.com>
2019-08-25 22:54:03 -0400

    class ref

--- analysis/cls.py
+++ analysis/cls.py
@@ -7,9 +7,3 @@ class Test:
 
     def testy(data1, data2):
         df = pd.DataFrame({'a': data1, 'b': data2})
-        return(df)
-    # def __init__(self, data):
-    #     self.data = data
-    #
-    # def frame_it(self):
-        # return pd.DataFrame(data)
