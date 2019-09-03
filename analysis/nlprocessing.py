9565eda9291c84743ced2e0038864e2cb08cfece
kevcon <kevscon@gmail.com>
2019-09-02 21:21:32 -0400

    nlp class

--- analysis/nlprocessing.py
+++ analysis/nlprocessing.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 # print top words of topic model
@@ -40,39 +41,84 @@ def prcntg(doc_top_df):
     return(prcntg_df)
 
 
-class NLProcess:
+class TopClass:
 
-    def __init__(self, doc_tops, top_labels, doc_labels):
+    def __init__(self, doc_tops):
+        # array of topic weights for each document
         self.doc_tops = doc_tops
-        self.top_labels = top_labels
-        self.doc_labels = doc_labels
 
-    def top_wgts(self):
+    def top_wgts(self, top_labels='', doc_labels=''):
         '''
-        create dataframe of documents x weights
+        return dataframe of documents x weights
 
         Parameters
         ----------
-        doc_tops : array of topic weights for each document
-        top_labels : dictionary of topic #: topic label
+        top_labels : list of topic description labels
         doc_labels : list of document labels
         '''
-        # create dataframe of topic weights
+        # create dataframe object of documents x weights
         self.top_df = pd.DataFrame(self.doc_tops).copy()
-        # map topic # to topic labels and set as columns
-        self.top_df.columns = self.top_df.columns.map(self.top_labels)
-        # set index to document labels
-        self.top_df.set_index(self.doc_labels, inplace=True)
+        if top_labels:
+            # set columns as topic labels
+            self.top_df.columns = top_labels
+        if doc_labels:
+            # set index to document labels
+            self.top_df.set_index([doc_labels], inplace=True)
         # return weight dataframe
         return(self.top_df)
 
-    def prcntg(self):
+    def top_pct(self, top_labels='', doc_labels=''):
         '''
-        convert weights to percentage of topic
+        convert topic weights to percentage of document
+
+        Parameters
+        ----------
+        top_labels : list of topic description labels
+        doc_labels : list of document labels
         '''
-        # convert weights to percentage
-        self.prcntg_df = self.doc_top_df.div(self.doc_top_df.sum(axis=1), axis=0).copy()
-        # drop any NaN values from dataframe
-        self.prcntg_df.dropna(inplace=True)
+        # create dataframe object of documents x weights
+        self.top_df = pd.DataFrame(self.doc_tops).copy()
+        # convert weights to proportion
+        self.top_df = self.top_df.div(self.top_df.sum(axis=1), axis=0).copy()
+        # round to percentage
+        self.top_df = self.top_df.apply(lambda x: round(x * 100, 1))
+        if top_labels:
+            # set columns as topic labels
+            self.top_df.columns = top_labels
+        if doc_labels:
+            # set index to document labels
+            self.top_df.set_index([doc_labels], inplace=True)
         # return percentage dataframe
-        return(self.prcntg_df)
+        return(self.top_df)
+
+    def sort_tops(self, doc_labels=''):
+        '''
+        sort topics by weight per document in descending order
+
+        Parameters
+        ----------
+        doc_labels : list of document labels
+        '''
+        # dataframe of weight values
+        sort_val = self.top_df.values.copy()
+        # sort rows by value (ascending order)
+        sort_val.sort(axis=1)
+        # reverse values in rows (descending order)
+        sort_val = np.flip(sort_val, axis=1)
+        # create dataframe
+        sort_val = pd.DataFrame(sort_val)
+        # sort labels by weight
+        sort_lab = pd.DataFrame(self.top_df.columns[np.argsort(-self.top_df.values,
+            axis=1)])
+        # concatenate dataframes and interweave columns
+        sort_df = pd.concat([sort_lab, sort_val], axis=1)[np.arange(len(self.top_df.columns))]
+        # create hierarchical column names
+        multi_idx = pd.MultiIndex.from_product([np.arange(len(self.top_df.columns)),
+            ['Label', 'Weight']])
+        # set column labels
+        sort_df = pd.DataFrame(sort_df.values, columns=multi_idx)
+        if doc_labels:
+            # set index to document labels
+            sort_df.set_index([doc_labels], inplace=True)
+        # return dataframe
+        return(sort_df)
